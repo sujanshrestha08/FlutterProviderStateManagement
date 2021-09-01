5b8bd29fd8e07e359f499b52d3fda8011c6b9580
Sujan Shrestha <sujanstha406@gmail.com>
2021-09-01 19:18:29 +0545

    Learned Basic Flutter Provider State Management

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
     debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Timer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
